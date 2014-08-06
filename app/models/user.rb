ee6aa4418b9958715bdac5969a1f9278905b4421
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-08-06 15:06:20 +0200

    User email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, uniqueness: true, presence: true
+  validates_length_of :password, minimum: 3, message: "password must be at least 3 characters long", if: :password
+  validates_confirmation_of :password, message: "should match confirmation", if: :password
+
 end
