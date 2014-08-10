0dd69548675ee7b0b61740daea99bc8d2bc81717
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-08-10 10:21:09 +0200

    Staging mailer settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
 end
