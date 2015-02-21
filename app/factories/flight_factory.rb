62f2d50640184b1711881c94040f754d6dfa72b9
kkuc <kkuc93@bitbucket.org>
2015-02-21 17:04:06 +0100

    flight abstract factory structure changes

--- app/factories/flight_factory.rb
+++ app/factories/flight_factory.rb
@@ -1,11 +1,4 @@
-module FlightAbstractFactory
-	def create(user,departure_airport,arrival_airport,departure_date)
-		raise NotImplementedError, "Not implemented error"
-	end
-end
-
-class FlightFactory
-	include FlightAbstractFactory
+class FlightFactory < FlightAbstractFactory
 	def create(user,departure_airport,arrival_airport,departure_date)
 		
 	end
