6adccd9e6f03a9262c1e189f7120e82ef67bb911
kkuc <kkuc93@bitbucket.org>
2015-02-21 18:00:22 +0100

    flight factory create method implementation

--- app/factories/flight_factory.rb
+++ app/factories/flight_factory.rb
@@ -1,5 +1,42 @@
 class FlightFactory < FlightAbstractFactory
-	def create(user,departure_airport,arrival_airport,departure_date)
+	attr_accessor :user, :departure_airport, :destination_airport, :date, :flight
+
+	def initialize(user, departure_airport, destination_airport, date)
+		@user = user
+		@departure_airport = departure_airport
+		@destination_airport = destination_airport
+		@date = date	
+	end
+	
+	def create
+		price_fetcher = Wizzair::Fetchers::Price.new(departure_airport, destination_airport, date)
+		@response = price_fetcher.call
+		if @response.success?
+			create_or_find_airports
+			create_flight
+			update_price
+		end
+		flight
+	end
+
+	private 
+
+	def create_or_find_airports
+		@departure_airport = Airport.where(name: departure_airport).first_or_create
+		@destination_airport = Airport.where(name: destination_airport).first_or_create
+	end
+
+	def create_flight
+		@flight = Flight.new
+		flight.user = user
+		flight.departure_airport = departure_airport
+		flight.destination_airport = destination_airport
+		flight.departure_date = @response[:departure_date]
+		flight.arrival_date = @response[:arrival_date]
+	end
+
+	def update_price
 		
 	end
+
 end
