162c66fa3ae5112d09a59c59ece8a87905aab06d
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-02-24 20:27:10 +0100

    Add notification to flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,10 +5,15 @@ class Flight < ActiveRecord::Base
   has_one :airline, through: :departure_airport
 
   has_many :prices, dependent: :destroy
+  has_many :notifications
 
   scope :with_name, -> (name) { where('lower(name) ILIKE ?', name.downcase) }
   scope :with_full_name, -> (name) { where('lower(full_name) ILIKE ?', name.downcase) }
 
   validates_presence_of :user
+
+  def current_price
+    prices.last
+  end
 
 end
